f56dd8de23096733139133b378fe2f398efb04ba
Carine Lacroix <callacroix@gmail.com>
2022-10-31 11:22:43 +0100

    create object to find random baby name

---
+++ app/src/main/java/app/ll/baby_name/BabyNames.java
@@ -0,0 +1,9 @@
+public class BabyNames {
+    private Map<SexEnum, BabyTypeNames> namesByType;
+    // Getters and setters
+}
+
+public class BabyTypeNames {
+    private List<String> names;
+    // Getters and setters
+}
