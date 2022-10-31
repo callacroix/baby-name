620d08b13eb5a2e1c9b6bf261f21199ab656ece0
Carine Lacroix <callacroix@gmail.com>
2022-10-31 11:15:24 +0100

    get random name (#8)

---
+++ app/src/main/java/app/ll/baby_name/RandomNameService.java
@@ -0,0 +1,26 @@
+package app.ll.baby_name;
+
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class RandomNameService {
+    private static final String TAG = "MY_APP_DEBUG_TAG";
+    private Handler handler; // handler that gets info from Bluetooth service
+  
+    private String fileName;
+  
+    public RandomNameService(String file) {
+       this.fileName = file;
+    }
+
+    public String getRandomName(SexEnum type) {
+      InputStream inputStream = new FileInputStream(new File(fileName));
+      
+      Yaml yaml = new Yaml();
+      Map<String, Object> data = yaml.load(inputStream);
+      System.out.println(data);
+    }
+}
